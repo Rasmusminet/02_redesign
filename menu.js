dfbde5247664747ef306c86c6c830dacc1777b2e
rasmus <rari0001@stud.kea.dk>
2023-05-23 11:12:02 +0200

    ny burger

--- menu.js
+++ menu.js
@@ -1,1 +1,8 @@
+var burgerMenu = document.getElementById("burger-menu");
 
+var overlay = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});

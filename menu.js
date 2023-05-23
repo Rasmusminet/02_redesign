610b4ea938651fa4968106c15985636992ee60d3
Emma <emmabille16@gmail.com>
2023-05-23 13:02:03 +0200

    popup

--- menu.js
+++ menu.js
@@ -1,1 +1,25 @@
+function showPopup(title, description, spotifyLink, imageUrl) {
+  var popup = document.getElementById("popup");
+  var popupImage = document.getElementById("popup-image");
+  var popupTitle = document.getElementById("popup-title");
+  var popupDescription = document.getElementById("popup-description");
+  var spotifyLinkElement = document.getElementById("spotify-link");
 
+  popupImage.src = imageUrl;
+  popupTitle.textContent = title;
+  popupDescription.textContent = description;
+  spotifyLinkElement.href = spotifyLink;
+
+  popup.style.display = "block";
+
+  var closeBtn = document.getElementById("close-btn");
+  closeBtn.onclick = function () {
+    popup.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  };
+}

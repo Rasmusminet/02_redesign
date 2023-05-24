4825206ab36031fb6626b42a46e4b7ca86b42679
Kezia <keha0001@stud.kea.dk>
2023-05-24 12:55:21 +0200

    tilføjet popup

--- menu.js
+++ menu.js
@@ -35,3 +35,26 @@ function showPopup(title, description, spotifyLink, imageUrl) {
     }
   };
 }
+
+/********************* pop-up til om Alive ************************* */
+
+// Get the necessary elements
+const popup = document.getElementById("popup");
+const popupImageContent = document.getElementById("popupImageContent");
+const closePopup = document.getElementById("closePopup");
+const image = document.getElementById("popupImage");
+
+// Function to open the popup
+function openPopup() {
+  popup.style.display = "block";
+  popupImageContent.src = image.src;
+}
+
+// Function to close the popup
+function close() {
+  popup.style.display = "none";
+}
+
+// Attach event listeners
+image.addEventListener("click", openPopup);
+closePopup.addEventListener("click", close);

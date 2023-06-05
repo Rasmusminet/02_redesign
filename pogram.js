f14629f1f6d55805f368a5bb6cfb3f797afbf33d
rasmus <rari0001@stud.kea.dk>
2023-06-05 20:51:33 +0200

    nye java

--- pogram.js
+++ pogram.js
@@ -23,3 +23,29 @@ function showPopup(title, description, iframeLink, imageUrl) {
     }
   };
 }
+/******************popup */
+function showPopup(title, description, spotifyLink, imageUrl) {
+  var popup = document.getElementById("popup");
+  var popupImage = document.getElementById("popup-image");
+  var popupTitle = document.getElementById("popup-title");
+  var popupDescription = document.getElementById("popup-description");
+  var spotifyLinkElement = document.getElementById("spotify-link");
+
+  popupImage.src = imageUrl;
+  popupTitle.textContent = title;
+  popupDescription.textContent = description;
+  spotifyLinkElement.href = spotifyLink;
+
+  popup.style.display = "block";
+
+  var closeBtn = document.getElementById("close-btn");
+  closeBtn.onclick = function () {
+    popup.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  };
+}

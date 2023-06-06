42ee31986ad320e5e42caf0fb5fab9a0c64560e7
rasmus <rari0001@stud.kea.dk>
2023-06-06 10:38:31 +0200

    ff

--- pogram.js
+++ pogram.js
@@ -3,40 +3,12 @@ function showPopup(title, description, iframeLink, imageUrl) {
   var popupImage = document.getElementById("popup-image");
   var popupTitle = document.getElementById("popup-title");
   var popupDescription = document.getElementById("popup-description");
-  var iframelink = document.getElementById("iframe-popup");
+  var iframeLink = document.getElementById("iframe-popup");
 
   popupImage.src = imageUrl;
   popupTitle.textContent = title;
   popupDescription.textContent = description;
-  iframelink.src = iframeLink;
-
-  popup.style.display = "block";
-
-  var closeBtn = document.getElementById("close-btn");
-  closeBtn.onclick = function () {
-    popup.style.display = "none";
-  };
-
-  window.onclick = function (event) {
-    if (event.target == popup) {
-      popup.style.display = "none";
-    }
-  };
-}
-/******************popup */
-function showPopup(title, description, spotifyLink, imageUrl) {
-  var popup = document.getElementById("popup");
-  var popupImage = document.getElementById("popup-image");
-  var popupTitle = document.getElementById("popup-title");
-  var popupDescription = document.getElementById("popup-description");
-  var spotifyLinkElement = document.getElementById("spotify-link");
-
-  popupImage.src = imageUrl;
-  popupTitle.textContent = title;
-  popupDescription.textContent = description;
-  spotifyLinkElement.href = spotifyLink;
-
-  popup.style.display = "block";
+  iframeink.src = iframeLink;
 
   var closeBtn = document.getElementById("close-btn");
   closeBtn.onclick = function () {
